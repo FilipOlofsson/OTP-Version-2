63c37e94c0d6498f45897bf4c108013fb5582b03
filipolofsson <te15.filip.olofsson@umea.nti.se>
2017-10-04 14:45:02 +0200

    Encrypt and Decrypt Algorithm improved

--- src/Crypto.java
+++ src/Crypto.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 class Crypto {
 
-    static void Perform(File input, File key, File output) throws IOException {
+    static void Encrypt(File input, File key, File output) throws IOException {
         if(input.length() > key.length()) {
             throw new IOException("Input file is shorter than key file.");
         }
@@ -14,6 +14,22 @@ class Crypto {
         DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(output));
 
         while((InputASCII = inputIn.read()) != -1 && (KeyASCII = keyIn.read()) != -1) {
+            outputStream.write(InputASCII ^ KeyASCII);
+        }
+    }
+
+    static void Decrypt(File input, File key, File output) throws IOException {
+        if(input.length() > key.length()) {
+            throw new IOException("Input file is shorter than key file.");
+        }
+        DataInputStream encryptedIn = new DataInputStream(new FileInputStream(input));
+        BufferedReader keyIn = new BufferedReader(new FileReader(key));
+
+        int KeyASCII, InputASCII;
+
+        DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(output));
+
+        while((InputASCII = encryptedIn.read()) != -1 && (KeyASCII = keyIn.read()) != -1) {
             outputStream.write(InputASCII ^ KeyASCII);
         }
     }
--- src/CryptographyController.java
+++ src/CryptographyController.java
@@ -4,24 +4,12 @@ import java.io.IOException;
 class CryptographyController {
 
     void Perform(Arguments args, String Input, String Key, String Output) {
-        if(args == Arguments.Decrypt) {
-            Decrypt(Input, Key, Output);
-        } else {
-            Encrypt(Input, Key, Output);
-        }
-    }
-
-    private void Encrypt(String Input, String Key, String Output) {
         try {
-            Crypto.Perform(new File(Input), new File(Key), new File(Output));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void Decrypt(String Input, String Key, String Output) {
-        try {
-            Crypto.Perform(new File(Input), new File(Key), new File(Output));
+            if(args == Arguments.Decrypt) {
+                Crypto.Decrypt(new File(Input), new File(Key), new File(Output));
+            } else {
+                Crypto.Encrypt(new File(Input), new File(Key), new File(Output));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
